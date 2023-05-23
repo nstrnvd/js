dccc36e98b33b1328108969e0380dbc08a10832b
nstrnvd <nastaranvalizade2@gmail.com>
2023-05-23 11:30:58 +0330

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+//Exercise one//
+let text = "Hello World!";
+let uppercase = text.toUpperCase();
+console.log(uppercase);
+
+//Exercise two//
+let fullName = 'Sebastian Bach';
+let newName = fullName.slice(1,9);
+console.log(newName);
+
+//Exercise three//
+console.log(0.1 + 0.8);
+console.log(0.1 + 0.2 == 0.3);
+console.log(0.1 + 0.2 === '0.3');
+
+//Exercise four//
+let color = ["red", "Orange", "blue", "yellow"];
+let length = color.length;
+console.log(length);
+
+//Exercise five//
+var myArr = [1, 2, 3, 4];
+myArr[2] = 8; 
+console.log(myArr,length);
+
+//Exercise six//
+let myArray = ["v", "s", "g", "h","d","t","w","q","y","u"];
+let array = myArray.length;
+console.log(myArray[0],array);
+
+//Exercise seven//
+let myarray = [1, 3, 'hey', 'how you doin'];
+myarray[2]= 'wow';
+console.log(myarray);
+
+//Exercise eight//
+const title = "I have often dreamed of a far off place";
+const title2 = "Where a hero's welcome";
+const title3 = "would be waiting for me";
+
+const number = title.concat(title2, title3);
+console.log(number);
+
+//Exercise nine//
+let neWarray = [true, 2, 3, "9", 8];
+neWarray.splice(3,1);
+console.log(neWarray[3]);
+
+//Exercise then
+var a1 = "true";
+var a2 = "false";
+var a3 = false ;
+a1 + a2 + a3;
+console.log(a1,a2,a3);
+
+
+
+
+
+
+
+
+
+
+
+
